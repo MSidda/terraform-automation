206d82e1cc74f89f57cf1b0a18eac623e39c99f7
MSidda <42439015+MSidda@users.noreply.github.com>
2020-11-11 15:32:07 +0530

    Update task-schduler.ps1

--- task-schduler.ps1
+++ task-schduler.ps1
@@ -1,5 +1,5 @@
 $repeat = (New-TimeSpan -Minutes 5)
 $Trigger= New-ScheduledTaskTrigger -Once -At 11:19am -RepetitionInterval $repeat # Specify the trigger settings
-$User= "winvm\siddareddy" # Specify the account to run the script
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\Users\siddareddy\Documents\vm.ps1" # Specify what program to run and with its parameters
+$User= "$username" # Specify the account to run the script
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "$path" # Specify what program to run and with its parameters
 Register-ScheduledTask -TaskName "copytask" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force # Specify the name of the task
